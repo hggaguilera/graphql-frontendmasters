4c659900340178e42cbac03c02291bfecfe74b87
Gabriel Gonzalez <hgglez92@gmail.com>
2020-05-20 09:29:36 -0600

    handling server error

--- advanced-gql-v2/server.js
+++ advanced-gql-v2/server.js
@@ -5,93 +5,104 @@ const pubSub = new PubSub();
 const NEW_ITEM = "NEW_ITEM";
 
 const typeDefs = gql`
-   type User {
-      id: ID!
-      username: String!
-      createdAt: Int!
-   }
-   type Settings {
-      user: User!
-      theme: String
-   }
+  type User {
+    id: ID!
+    username: String!
+    error: String
+    createdAt: Int!
+  }
+  type Settings {
+    user: User!
+    theme: String
+  }
 
-   type Item {
-      task: String!
-   }
+  type Item {
+    task: String!
+  }
 
-   input NewSettingsInput {
-      user: ID!
-      theme: String!
-   }
+  input NewSettingsInput {
+    user: ID!
+    theme: String!
+  }
 
-   type Query {
-      me: User!
-      settings(user: ID!): Settings!
-   }
+  type Query {
+    me: User!
+    settings(user: ID!): Settings!
+  }
 
-   type Mutation {
-      settings(input: NewSettingsInput!): Settings!
-      createItem(task: String!): Item!
-   }
+  type Mutation {
+    settings(input: NewSettingsInput!): Settings!
+    createItem(task: String!): Item!
+  }
 
-   type Subscription {
-      newItem: Item
-   }
+  type Subscription {
+    newItem: Item
+  }
 `;
 
 const resolvers = {
-   Query: {
-      me() {
-         return {
-            id: 1,
-            username: "hggonzalez",
-            createdAt: "122454546",
-         };
-      },
-      settings(_, { user }) {
-         return {
-            user,
-            theme: "Light",
-         };
-      },
-   },
-   Mutation: {
-      settings(_, { input }) {
-         return input;
-      },
-      createItem(_, { task }) {
-         const item = { task };
-         pubSub.publish(NEW_ITEM, { newItem: item });
-         return item;
-      },
-   },
-   Subscription: {
-      newItem: {
-         subscribe: () => pubSub.asyncIterator(NEW_ITEM),
-      },
-   },
-   Settings: {
-      user() {
-         return {
-            id: 1,
-            username: "hggonzalez",
-            createdAt: "122454546",
-         };
-      },
-   },
+  Query: {
+    me() {
+      return {
+        id: 1,
+        username: "hggonzalez",
+        createdAt: "122454546",
+      };
+    },
+    settings(_, { user }) {
+      return {
+        user,
+        theme: "Light",
+      };
+    },
+  },
+  Mutation: {
+    settings(_, { input }) {
+      return input;
+    },
+    createItem(_, { task }) {
+      const item = { task };
+      pubSub.publish(NEW_ITEM, { newItem: item });
+      return item;
+    },
+  },
+  Subscription: {
+    newItem: {
+      subscribe: () => pubSub.asyncIterator(NEW_ITEM),
+    },
+  },
+  Settings: {
+    user() {
+      return {
+        id: 1,
+        username: "hggonzalez",
+        createdAt: "122454546",
+      };
+    },
+  },
+  User: {
+    error() {
+      throw new Error("You cant do this");
+    },
+  },
 };
 
 const server = new ApolloServer({
-   typeDefs,
-   resolvers,
-   context({ connection, req }) {
-      if (connection) {
-         return { ...connection.context };
-      }
-   },
-   subscriptions: {
-      onConnect(params) {},
-   },
+  typeDefs,
+  resolvers,
+  formatError(error) {
+    console.log(error);
+    // return new Error('This is a custom error');
+    return error;
+  },
+  context({ connection, req }) {
+    if (connection) {
+      return { ...connection.context };
+    }
+  },
+  subscriptions: {
+    onConnect(params) {},
+  },
 });
 
 server.listen().then(({ url }) => console.log(`server at ${url}`));
